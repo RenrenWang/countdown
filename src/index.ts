8e89824b714e3e66fcd56c2dcfc29ed95576c090
RenrenWang <1011420784@QQ.COM>
2023-07-12 20:21:20 +0800

    feat: 代码优化

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,36 @@
  * 倒计时组件
  * requestAnimationFrame 实现
  */
-type timeType = {
+export type timeType = {
   second: number; // 剩余秒数
   time: number; // 剩余毫秒
 };
-interface CountdownProps {
+export  interface CountdownProps {
   time: number; // 倒计时秒数
   callback?: (result: timeType) => void; // 倒计时回调
   onEnd?: () => void; // 结束回调
 }
 const Countdown = ({ time, callback, onEnd }: CountdownProps) => {
-  const endTime = new Date().getTime() + time * 1000;
+  const endTime = new Date().getTime() + (time-1) * 1000;
   let id = 0;
-  const m = () => {
+  const handle = () => {
     const now = new Date().getTime();
-    if (endTime > now) {
-      const _time = (endTime - now) / 1000;
-      const second = Math.ceil(_time);
-      callback &&
-        callback({
-          second,
-          time: _time,
-        });
-      id = requestAnimationFrame(m);
+    const currentTime = (endTime - now) / 1000;
+    const second = Math.ceil(currentTime);
+    callback &&
+    callback({
+      second,
+      time: currentTime,
+    });
+    if (endTime >= now) {
+      id = requestAnimationFrame(handle);
     } else {
       onEnd && onEnd();
       id && cancelAnimationFrame(id);
     }
+  
   };
-  id = requestAnimationFrame(m);
+  id = requestAnimationFrame(handle);
   return () => {
     id && cancelAnimationFrame(id);
   };
